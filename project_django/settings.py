827beb323261957fd28ad66cc324f9a3fabae745
Syadza Desvianto <syadzadesvianto@gmail.com>
2022-10-06 10:04:54 +0700

    Tugas 5

--- project_django/settings.py
+++ project_django/settings.py
@@ -43,7 +43,13 @@ INSTALLED_APPS = [
     'katalog',
     'mywatchlist',
     'todolist',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
